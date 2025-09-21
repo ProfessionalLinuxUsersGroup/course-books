6948f49ba9b2c87fd1d823ea2919eea28a6500d5
cmckee786 <mckee_christian@yahoo.com>
2025-09-21 14:44:07 -0400

    feat(scripts): add link validation and reporting
    - Roughly 30s-1m runtime. (GNU domain has rigorous scrape prevention?)
    - Spits out many metrics to stdout
    - Logs only failed links at this time

---
+++ scripts/link-validate.py
@@ -0,0 +1,118 @@
+# v 1.0
+# Authored by Christian McKee - cmckee786@github.com
+# Attempts to validate links within ProLUG Course-Books repo
+# Must be called from root of github repo directory
+# DO NOT use in runner builds for the time being
+
+import re
+from datetime import datetime
+import urllib.request
+import urllib.error
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+RED = "\033[91m"
+GREEN = "\033[92m"
+BLUE = "\033[34m"
+ORANGE = "\033[33m"
+RESET = "\033[0m"
+
+failed_links = []
+
+def link_validation(unique_link):
+    """Utilizes user-agent headers lest webservers return false negative
+       Odds are some requests may be blocked by rate limiters like Cloudflare
+    """
+    headers = {
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/140.0.0.0 Safari/537.36 Edg/134.0.0.0"
+        ),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Connection": "keep-alive",
+    }
+
+    req = urllib.request.Request(unique_link, headers=headers)
+
+    try:
+        with urllib.request.urlopen(req, timeout = 30) as response:
+            if response.code >= 200 or response.code <=399:
+                print(
+                    f'{unique_link}\n'
+                    f'\t- Responded {GREEN}{response.status} {response.reason}{RESET}'
+                )
+            else:
+                print(
+                    f'{unique_link}\n'
+                    f'\t- Unknown error {RED}[FAILED]{RESET}'
+                )
+    except urllib.error.HTTPError as e:
+        print(
+            f'{unique_link}\n'
+            f'\t- Responded HTTP Error: {RED}{e.code} {e.reason}{RESET}'
+        )
+        failed_links.append(unique_link)
+    except urllib.error.URLError as e:
+        print(
+            f'{unique_link}\n'
+            f'\t- Responded URL Error: {RED}{e.reason}{RESET}'
+        )
+        failed_links.append(unique_link)
+    except TimeoutError as e:
+        print(
+            f'{unique_link}\n'
+            f'\t- Responded {RED}Timeout Error{RESET}'
+        )
+        failed_links.append(unique_link)
+
+def main():
+    """Validates links found in ProLUG Course-Books repo
+        Returns per file total and total unique links found
+        Then reports invalidated links to file
+
+        Must be run from root of git repo directory
+    """
+
+    uniq_total = set()
+    total_links = int(0)
+    file_paths = Path('docs/').glob('**/*.md')
+
+    for path in file_paths:
+        with open(path, 'r', encoding='utf-8') as f:
+            contents = f.read()
+            link_search = re.findall(r"(?<!\[)https?:\/\/(?:\S+\.)[^\s\)\]\}\<\>\"\,]+", contents)
+            list_sum = len(link_search)
+            print(f'{ORANGE}File{RESET}: {path}\n{"-"*50}')
+            if list_sum > 0:
+                for link in link_search:
+                    print(link)
+                    uniq_total.add(link)
+                total_links += list_sum
+                print(f'\nLinks found: {BLUE}{list_sum}{RESET}\n')
+            else:
+                print('No links found...\n')
+    print(f'Total links found: {ORANGE}{total_links}{RESET}')
+    print(f'Unique links: {GREEN}{len(uniq_total)}{RESET}\n{"-"*50}')
+
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        futures = {executor.submit(link_validation, url): url for url in uniq_total}
+        for future in as_completed(futures):
+            try:
+                future.result()
+            except Exception as e:
+                print(f'{futures[future]} -> Unexpected error: {e}')
+
+    report = f"'./failed_links.report.{datetime.now().strftime('%Y-%m-%d')}'"
+    with open(
+        report,
+        'w',
+        encoding='utf-8') as f:
+
+        print('Writing report to file...')
+        f.write(f'Report ran on: {datetime.now().strftime("%Y-%m-%d %H:%M")}\n{"-"*50}\n')
+        [f.writelines(f'{link}\n') for link in failed_links]
+
+if __name__ == '__main__':
+    main()
