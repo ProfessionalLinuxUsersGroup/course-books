e57f78c268d438aef163abb57840745631ffcc8f
cmckee786 <mckee_christian@yahoo.com>
2025-09-22 14:22:24 -0400

    refactor(scripts): link-validate.py changes
    - implement dictionaries for more thorough reporting
    - refactor and streamline reporting from new dicts
    - implement re.search via splitlines and enumeration of files
    - shift reporting logic outside of business logic

--- scripts/link-validate.py
+++ scripts/link-validate.py
@@ -1,4 +1,4 @@
-# v 1.0.1
+# v 1.1.87
 # Authored by Christian McKee - cmckee786@github.com
 # Attempts to validate links within ProLUG Course-Books repo
 # Must be called from root of github repo directory
@@ -24,7 +24,7 @@ REGEX = r"(?<!\[)https?:\/\/(?:\S+\.)[^\s\)\]\}\<\>\"\,]+"
 
 failed_links = []
 
-def link_validation(unique_link):
+def link_validation(matched_item: dict):
     """Utilizes user-agent headers lest webservers return false negative
        Odds are some requests may be blocked by rate limiters like Cloudflare
     """
@@ -37,41 +37,51 @@ def link_validation(unique_link):
         "Connection": "keep-alive",
     }
 
-    req = urllib.request.Request(unique_link, headers=headers)
+    req = urllib.request.Request(matched_item["link"], headers=headers)
+    troubleshoot_output = f' {ORANGE}File:{RESET}{matched_item["file"]}'\
+                          f' {BLUE}L:{matched_item["line"]} {RESET}'
 
     try:
-        with urllib.request.urlopen(req, timeout = 15) as response:
+        with urllib.request.urlopen(req, timeout = 7) as response:
             if response.code >= 200 or response.code <=399:
                 print(
-                    f'{unique_link}\n'
+                    f'{matched_item['link']}\n'
                     f'\t- Responded {GREEN}{response.status} {response.reason}{RESET}'
                 )
             else:
                 print(
-                    f'{unique_link}\n'
+                    f'{matched_item['link']}\n'
                     f'\t- Unknown error {RED}[FAILED]{RESET}'
                 )
-                failed_links.append(unique_link)
+                failed_links.append(
+                    f'{matched_item["link"]}{troubleshoot_output}'
+                )
     except urllib.error.HTTPError as e:
         print(
-            f'{unique_link}\n'
+            f'{matched_item["link"]}\n'
             f'\t- Responded HTTP Error: {RED}{e.code} {e.reason}{RESET}'
         )
-        failed_links.append(f'{unique_link} - {e.code} {e.reason}')
+        failed_links.append(
+            f'{matched_item['link']} - {RED}{e.code} {e.reason}{RESET}{troubleshoot_output}'
+        )
     except urllib.error.URLError as e:
         print(
-            f'{unique_link}\n'
+            f'{matched_item['link']}\n'
             f'\t- Responded URL Error: {RED}{e.reason}{RESET}'
         )
-        failed_links.append(f'{unique_link} - {e.reason}')
+        failed_links.append(
+            f'{matched_item['link']} - {RED}{e.reason}{RESET}{troubleshoot_output}'
+        )
     except TimeoutError as e:
         print(
-            f'{unique_link}\n'
+            f'{matched_item['link']}\n'
             f'\t- Responded {RED}Timeout Error{RESET}'
         )
-        failed_links.append(f'{unique_link} - Timeout Error')
+        failed_links.append(
+            f'{matched_item['link']} - {RED}Timeout Error{RESET}{troubleshoot_output}'
+        )
 
-def main():
+def link_processing():
     """Validates links found in ProLUG Course-Books repo
         Returns per file total and total unique links found
         Then reports invalidated links to file
@@ -79,34 +89,57 @@ def main():
         Must be run from root of git repo directory
     """
 
-    uniq_total = set()
+    matched_links = []
+    unique_links = []
+    file_matches = int(0)
     total_links = int(0)
     file_paths = Path('docs/').glob('**/*.md')
+    link_item = {
+        "link": "",
+        "file": "",
+        "line": ""
+    }
 
     for path in file_paths:
         with open(path, 'r', encoding='utf-8') as f:
-            contents = f.read()
-            link_search = re.findall(REGEX, contents)
-            list_sum = len(link_search)
             print(f'{ORANGE}File{RESET}: {path}\n{"-"*50}')
-            if list_sum > 0:
-                for link in link_search:
-                    print(link)
-                    uniq_total.add(link)
-                total_links += list_sum
-                print(f'\nLinks found: {BLUE}{list_sum}{RESET}\n')
-            else:
+            contents = f.read().splitlines()
+            for i, line in enumerate(contents, 1):
+                str_match = re.search(REGEX, line)
+                if str_match:
+                    print(f'{GREEN}L:{i}{RESET} | {str_match.group(0)}')
+                    link_item = {
+                        "link": str_match.group(0),
+                        "file": f"{path.parent}/{path.name}",
+                        "line": i
+                    }
+                    matched_links.append(link_item)
+                    file_matches += 1
+            if file_matches == 0:
                 print('No links found...\n')
+            else:
+                print(f'\nLinks found: {BLUE}{file_matches}{RESET}\n')
+            total_links += file_matches
+            file_matches = 0
+    unique_links = list({i['link']:i for i in reversed(matched_links)}.values())
+
     print(f'Total links found: {ORANGE}{total_links}{RESET}')
-    print(f'Unique links: {GREEN}{len(uniq_total)}{RESET}\n{"-"*50}')
+    print(f'Unique links: {GREEN}{len(unique_links)}{RESET}\n{"-"*50}')
 
     with ThreadPoolExecutor(max_workers=WORKER_COUNT) as executor:
-        futures = {executor.submit(link_validation, url): url for url in uniq_total}
+        futures = {
+            executor.submit(link_validation, dict_item):
+            dict_item for dict_item in unique_links
+        }
         for future in as_completed(futures):
             try:
                 future.result()
             except Exception as e:
                 print(f'{futures[future]} -> Unexpected error: {e}')
+
+def main():
+    """The place we call home"""
+    link_processing()
 
     report = f"failed_links.{datetime.now().strftime('%Y-%m-%d')}"
     with open(
