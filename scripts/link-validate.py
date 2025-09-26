9a730b329bd3a82b1a1b89968d237d7367251a8a
cmckee786 <mckee_christian@yahoo.com>
2025-09-26 19:25:12 -0400

    fix(scripts): missing user agent vaules, regex cleanup

--- scripts/link-validate.py
+++ scripts/link-validate.py
@@ -26,7 +26,7 @@ RESET = "\033[0m"
 # Worker count dependent on host limitations
 WORKER_COUNT = 20
 # Regex intended to match http(s) links unique to this project
-REGEX = r"(?<!\[)https?:\/\/\S+[^\s\.\"\'\,\>\)\{\}]"
+REGEX = r"(?<!\[)https?://\S+[^\s\.\"\'\,\>\)\{\}]"
 pattern = re.compile(REGEX)
 
 STORAGE = 'scripts/link-storage/successfullinks.txt'
@@ -58,7 +58,7 @@ def link_validation(matched_item: dict):
     """
     headers = {
         "User-Agent": (
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/143.0"
         ),
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
